947c60c06e750ac799b99d452b4a9d34baec2cd9
Samuel Ferrell <samuelcferrell@gmail.com>
2012-11-06 00:12:14 -0800

    move backend stuff into subdir fetch js deps remotely

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,13 @@
 require.config({
 	baseUrl: './',
+	paths: {
+		"jquery": "http://code.jquery.com/jquery-1.8.2.min",
+		"underscore": "http://underscorejs.org/underscore-min",
+		"d3": "http://d3js.org/d3.v2.min"
+	},
 	waitSeconds: 15
 });
 
-require(["jquery"], function ($) {
-	$.getJSON("story/homeworld.json", function(data) {
-		
-	});
+require(["jquery", "underscore", "d3"], function () {
+	console.log($, _, d3);
 });
