be3b385b3c2237cef3d158e10d34f347e9257d5b
Samuel Ferrell <samuelcferrell@gmail.com>
2012-11-06 23:48:34 -0800

    generate dom from json

--- public/js/main.js
+++ public/js/main.js
@@ -9,5 +9,45 @@ require.config({
 });
 
 require(["jquery", "underscore", "d3"], function () {
-	console.log($, _, d3);
+	template_set        = "<div class='set' id='<%= questionID %>'><div class='query'><%= query %></div><%= choices %></div>";
+	template_choice_set = "<ol class='choice-list'><%= choices %></ol>";
+	template_question   = "<div class='question'><%= query %></div>";
+	template_choice     = "<li class='answer'><a href='#<%= destination %>' class='choicetext'><%= choice %></a><div class='eventtext'><%= eventtext %></div></li>";
+
+	$.getJSON("story/homeworld.json", function (story) {
+		
+		// Loop through questions
+		var i;
+		for (i = 0; i < story.Questions.length; i += 1) {
+			var compiled = "";
+			var question = story.Questions[i];
+			var j;
+
+			// Loop through choices
+			if (question.Choices instanceof Array) {
+				var set_string = "";
+				for (j = 0; j < question.Choices.length; j += 1) {
+					var set = question.Choices[j];
+
+					set_string += _.template(template_choice, {
+						choice: set.Name,
+						destination: set.Destination,
+						eventtext: set.Event
+					});
+				}
+
+				compiled = _.template(template_choice_set, {
+					choices: set_string
+				});
+			}
+
+			template = _.template(template_set, {
+				questionID: question.Id,
+				query: question.Query,
+				choices: compiled,
+			});
+
+			$(template).appendTo('body');
+		}
+	});
 });
