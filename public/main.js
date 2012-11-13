1aa05ca5fca32a6507c96548c82ac7df57796751
Samuel Ferrell <samuelcferrell@gmail.com>
2012-11-12 20:24:02 -0500

    add ship status and history, clean up js

--- public/main.js
+++ public/main.js
@@ -1,24 +1,34 @@
 $(document).ready(function () {
 	$("#root").fadeIn(500);
-	$eventArea = $("#event-text");
+
+	var $militaryCount = $("#military-status"),
+	    $civilianCount = $("#civilian-status"),
+	    $history       = $("#history"),
+	    $eventArea     = $("#event-text");
 
 	$('a').click(function (event) {
 		event.preventDefault();
-
-		var choice = $(this),
-		    fromId = choice.attr("data-from"),
-		    toId   = choice.attr("data-to"),
-		    action = choice.attr("data-event");
+		var choice  = $(this),
+		    fromId  = choice.attr("data-from"),
+		    toId    = choice.attr("data-to"),
+		    action  = choice.attr("data-event"),
+		    milLoss = choice.attr("data-mil-loss"),
+		    civLoss = choice.attr("data-civ-loss");
 
 		var $from = $("#" + fromId),
 		    $to   = $("#" + toId);
 
-		choice.parent().remove();
-		$eventArea.text(">> " + action);
+		var currentCivilian = $civilianCount.text(),
+		    currentMilitary = $militaryCount.text();
 
-		if ($to.length) {
+		choice.parent().remove();
+		$militaryCount.text(currentMilitary - milLoss);
+		$civilianCount.text(currentCivilian - civLoss);
+		$eventArea.text(">> " + action);
+		if (toId) {
 			$from.fadeOut(0);
-			$to.fadeIn(600);
+			$to.fadeIn(800);
+			$("<li/>", {text: choice.text()}).appendTo($history);
 		}
-	})
-})
+	});
+});
